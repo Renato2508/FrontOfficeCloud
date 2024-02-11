99e7c4ddbcd29970ec955610bee9d49904853699
Renato2508 <119660851+Renato2508@users.noreply.github.com>
2024-02-11 16:37:17 +0300

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -8,28 +8,40 @@ const Navbar = () => {
 
   const handleListAnnonce = (e) => {
     e.preventDefault();
-    navigate('/HomePage', { state: { type: 1 } });
+    navigate('/FeaturedCars');
   };
 
   const handleMessagerie = (e) => {
     e.preventDefault();
-    navigate('/HomePage', { state: { type: 2 } });
+    if (localStorage.getItem('authToken') !== null) {
+      console.log("Navigating to MessagesPage");
+      navigate('/MessagesPage');
+    } else {
+        console.log("Navigating to Login");
+        navigate('/Login');
+    }
   };
 
   const handleFiltre = (e) => {
     e.preventDefault();
-    navigate('/HomePage', { state: { type: 3 } });
+    navigate('/CarFilters');
   };
 
   const handleAnnonceFavoris = (e) => {
     e.preventDefault();
-    navigate('/HomePage', { state: { type: 4 } });
+    navigate('/FavoriteCars');
   };
+
+  const handleLogin= (e) => {
+    e.preventDefault();
+    navigate('/Login');
+  };
+
 
   const handleDeconnexion = (e) => {
     e.preventDefault();
-    localStorage.setItem("auth",false);
-    navigate('/HomePage', { state: { type: 1 } });
+    localStorage.removeItem('authToken');
+    navigate('/FeaturedCars');
   };
 
   return (
@@ -41,6 +53,7 @@ const Navbar = () => {
           <li onClick={handleMessagerie}><p>Messagerie</p></li>
           <li onClick={handleFiltre}><p>Filtre Annonce</p></li>
           <li onClick={handleAnnonceFavoris}><p>Annonces Favorites</p></li>
+          <li onClick={handleLogin}><p>Login</p></li>
           <li onClick={handleDeconnexion}><p>Deconnexion</p></li>
           {/* Add more navbar elements if necessary */}
         </ul>
